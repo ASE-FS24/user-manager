ffa48fca2071970ee1d427efdc8b65ff8067e35b
Riccardo Carissimi <riccardo@carissimi.eu>
2024-03-14 16:22:26 +0100

    SCRUM-21: lambda support on localstack

--- init.sh
+++ init.sh
@@ -5,5 +5,5 @@ aws --endpoint-url=http://localhost:4566 lambda create-function \
     --function-name user-manager-lambda \
     --runtime java17 \
     --handler ch.nexusnet.usermanager.aws.LambdaHandler \
-    --zip-file fileb://./target/usermanager-0.0.1-SNAPSHOT.jar \
+    --zip-file fileb:///tmp/usermanager-0.0.1-SNAPSHOT.jar \
     --role arn:aws:iam::000000000000:role/DummyRole
